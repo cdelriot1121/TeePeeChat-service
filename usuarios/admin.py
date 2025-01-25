ee43b94e74dede91d254f6860a4bd02a40a381c0
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-01-25 17:35:09 -0500

    refactor: add usuarioAdmin to register admin.py

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Usuario
-# Register your models here.
-admin.site.register(Usuario)
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'correo', 'codigo_unico', 'fecha_registro')
